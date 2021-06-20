be57d9d527086ed111324c634d75b2fab36963b5
Daniel Comboni <daniel@laboremus.no>
2021-06-20 11:15:33 +0300

    added mappings for user and audit history

---
+++ Cypherlead.Api/Core/Configs/MappingConfigurations/AuditHistoryProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Cypherlead.Api.Models;
+using Cypherlead.Api.ViewModels;
+using Cypherlead.Api.ViewModels.CreateModels;
+using Cypherlead.Api.ViewModels.UpdateModels;
+
+namespace Cypherlead.Api.Core.Configs.MappingConfigurations
+{
+    public class AuditHistoryProfile : Profile
+    {
+        public AuditHistoryProfile()
+        {
+            CreateMap<AuditHistory, AuditHistoryCreateModel>().ReverseMap();
+            CreateMap<AuditHistory, AuditHistoryUpdateModel>().ReverseMap();
+            CreateMap<AuditHistory, AuditHistoryViewModel>().ReverseMap();
+        }
+    }
+}
