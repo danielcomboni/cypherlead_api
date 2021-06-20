3dff51e6946dad37428003a106bd5f28729fbc24
Daniel Comboni <daniel@laboremus.no>
2021-06-20 11:20:41 +0300

    added auto-detect dependency injections

---
+++ Cypherlead.Api/Core/Extensions/DependencyInjection.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Cypherlead.Api.Core.Extensions
+{
+    public static class DependencyInjection
+    {
+        private static bool ShouldSkip(MemberInfo type, IEnumerable<string> exclude)
+        {
+            var exists = exclude.Any(it => type.Name.Contains(it));
+            return exists;
+        }
+
+        public static void ConfigureDependencyInjection(this IServiceCollection services, params Assembly[] assemblies)
+        {
+            var skip = new HashSet<string>
+            {
+                "GeneralService",
+                "GeneralRepository"
+            };
+
+            foreach (var assembly in assemblies)
+            {
+                var types = assembly
+                    .GetExportedTypes()
+                    .Where(it => it.IsClass && !ShouldSkip(it, skip))
+                    .Select(it =>
+                    {
+                        // get interface class, with similar name
+                        var inter = it.GetInterfaces()
+                            ?.Where(ot => ot.Name.Contains(it.Name))
+                            .FirstOrDefault();
+                        // Return a pair
+                        return (_class: it, _interface: inter);
+                    })
+                    //Remove types without interfaces
+                    .Where(it => it._interface != null);
+
+                foreach (var (_class, _interface) in types)
+                {
+                    Console.WriteLine($@"Service: {_class.FullName} impl:{_interface.FullName}");
+                    services.AddScoped(_interface, _class);
+                }
+            }
+        }
+    }
+}
